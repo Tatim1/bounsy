f9576daef546a200255e6ef837589806b8ea7fa1
Tatim1 <abrabd98@gmail.com>
2018-03-08 23:45:59 +0200

    1commit

---
+++ js/src/scripts.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+//  $('.team__slider').slick({
+//   dots: true,
+//   infinite: true,
+//   arrows: false,
+//   speed: 300,
+//   slidesToShow: 1,
+//   slide: ".team__slider-item"
+// });
+
+// $('.testimonials__slider').slick({
+//   dots: true,
+//   arrows: false,
+//   infinite: true,
+//   speed: 300,
+//   autoplay: true,
+//   autoplaySpeed: 4000,
+//   slidesToShow: 1,
+//   slide: ".testimonials__slider-item"
+// });
+
+// });
